44bfdf9ac285cd4c873db80dee4595234b9af39f
MarkZakelj <markzakelj@gmail.com>
2022-11-28 17:18:40 +0100

    sample code

---
+++ src/main/java/team/globaloptima/Customer.java
@@ -0,0 +1,49 @@
+package team.globaloptima;
+
+public class Customer {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
