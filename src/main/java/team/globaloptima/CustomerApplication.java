87d05959f52bfcba9f8231a6d91a785bdba8284b
MarkZakelj <markzakelj@gmail.com>
2023-01-05 03:28:08 +0100

    graphql update with rest

---
+++ src/main/java/team/globaloptima/CustomerApplication.java
@@ -0,0 +1,11 @@
+package team.globaloptima;
+
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("v1")
+public class CustomerApplication extends Application{
+
+}
